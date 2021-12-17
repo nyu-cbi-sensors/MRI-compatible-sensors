0c5661e1fafb4f116ecd786195b785739abe966c
lpancoast-nyu <64980668+lpancoast-nyu@users.noreply.github.com>
2021-12-17 14:36:23 -0500

    Create Credentials_demo.h

---
+++ mqtt_bno08x_i2c/include/Credentials_demo.h
@@ -0,0 +1,10 @@
+// For sensors to connect to RPi better create a Credentials.h file using following template
+
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+#define WIFI_SSID "[SSID]"
+#define WIFI_PASSWORD "[PASSWORD]"
+#define MQTT_SERVER "[ip address of server]"
+
+#endif
